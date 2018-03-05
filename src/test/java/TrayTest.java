44fcd77efaea59d4c202e4058944cc38a1f1fac2
Jim Bevington <jim.bevington@hotmail.com>
2018-03-05 13:31:18 +0000

    added setup and trayPrice test to TrayTest

--- src/test/java/TrayTest.java
+++ src/test/java/TrayTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
 public class TrayTest {
+
+    private Tray tray;
+
+    @Before
+    public void setUp() throws Exception {
+        tray = new Tray(0.50, 10);
+    }
+
+//    trays have a price
+
+    @Test
+    public void trayPrice() {
+        assertEquals(0.50, tray.getPrice(), 0.01);
+    }
+
+
+//    can report no. of items
+
+//    can add an item to a tray
+
+//    can remove an item
+
+//    can't add more than capacity
+
 }
