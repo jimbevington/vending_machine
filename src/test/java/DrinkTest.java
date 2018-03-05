bd71506c11d11ddf2adef3f01f572da22ee3bcda
Jim Bevington <jim.bevington@hotmail.com>
2018-03-05 11:53:40 +0000

    added setup method to DrinkTest

--- src/test/java/DrinkTest.java
+++ src/test/java/DrinkTest.java
@@ -1,2 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+
 public class DrinkTest {
+
+    private Drink drink;
+
+    @Before
+    public void setUp() throws Exception {
+        drink = new Drink("Pepsi", 330);
+    }
+
+
 }
+
