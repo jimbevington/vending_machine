f220fa1d6dc7419472fdc4d06d32974f77007712
Jim Bevington <jim.bevington@hotmail.com>
2018-03-05 12:15:11 +0000

    Setup Customer class and constructor

---
+++ src/main/java/Customer.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Customer {
+    private ArrayList<Product> inventory;
+    private double cash;
+    private String name;
+
+    public Customer(String name, double cash) {
+        this.name = name;
+        this.cash = cash;
+        this.inventory = new ArrayList<>();
+    }
+}
